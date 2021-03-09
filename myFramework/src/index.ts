65076258eb3f7b1fcac4eaca6e92d31bc82ba706
karuifeather <aryalaashaya@gmail.com>
2021-03-09 09:28:09 +0545

    very very basic model implementation

--- myFramework/src/index.ts
+++ myFramework/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello theredsadasdsa!');
+import { User } from './models/User';
+
+const user = new User({ name: 'Aashaya', age: 20 });
+
+console.log(user.get('name'));
+
+user.on('chan', () => {
+  console.log('eafds');
+});
+
+user.trigger('chan');
---
+++ myFramework/src/models/User.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+interface UserProps {
+  id?: number;
+  name?: string;
+  age?: number;
+}
+
+type Callback = () => void;
+
+export class User {
+  events: { [key: string]: Callback[] } = {};
+
+  constructor(private data: UserProps) {}
+
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+
+  set(changes: UserProps): void {
+    Object.assign(this.data, changes);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) return;
+
+    handlers.forEach((cb) => {
+      cb();
+    });
+  }
+
+  fetch(): void {
+    axios.get(`http://localhost:/users/${this.get('id')}`);
+  }
+}
