3ea0a0a747d48351ea0f977e07293aa987ffd020
karuifeather <aryalaashaya@gmail.com>
2021-03-09 07:30:43 +0545

    myFramework init commit

---
+++ myFramework/dist/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+console.log('Hello theredsadasdsa!');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9teUZyYW1ld29yay8uL3NyYy9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwiZmlsZSI6ImJ1bmRsZS5qcyIsInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdIZWxsbyB0aGVyZWRzYWRhc2RzYSEnKTtcclxuIl0sInNvdXJjZVJvb3QiOiIifQ==
