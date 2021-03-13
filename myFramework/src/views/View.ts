73ae35cc7801314c2e5c03e426f190c875f1d2f2
karuifeather <aryalaashaya@gmail.com>
2021-03-13 14:28:57 +0545

    Views nesting now enabled

--- myFramework/src/views/View.ts
+++ myFramework/src/views/View.ts
@@ -1,14 +1,23 @@
 import { Model } from '../models/Model';
 
 export abstract class View<T extends Model<K>, K> {
+  regions: { [key: string]: Element } = {};
+
   constructor(public parent: Element, public model: T) {
     this.model.on('change', (): void => {
       this.render();
     });
   }
 
-  abstract eventsMap(): { [key: string]: () => void };
   abstract template(): string;
+
+  regionsMap(): { [key: string]: string } {
+    return {};
+  }
+
+  eventsMap(): { [key: string]: () => void } {
+    return {};
+  }
 
   bindEvents(fragment: DocumentFragment): void {
     const eventsMap = this.eventsMap();
@@ -22,12 +31,31 @@ export abstract class View<T extends Model<K>, K> {
     }
   }
 
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap();
+
+    for (let key in regionsMap) {
+      const selector = regionsMap[key];
+      const element = fragment.querySelector(selector);
+
+      if (element) {
+        this.regions[key] = element;
+      }
+    }
+  }
+
+  onRender(): void {}
+
   render() {
     this.parent.innerHTML = '';
 
     const template = document.createElement('template');
     template.innerHTML = this.template();
+
     this.bindEvents(template.content);
+    this.mapRegions(template.content);
+
+    this.onRender();
 
     this.parent.append(template.content);
   }
