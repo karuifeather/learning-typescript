0c0b2a94aa512da26b1c00a809c34e9874fdaff7
karuifeather <aryalaashaya@gmail.com>
2021-03-29 11:38:32 +0545

    removing junk

--- myExpress/build/controllers/decorators/MetadataKeys.js
+++
@@ -1,7 +0,0 @@
-export var MetadataKeys;
-(function (MetadataKeys) {
-    MetadataKeys["method"] = "method";
-    MetadataKeys["path"] = "path";
-    MetadataKeys["middleware"] = "middleware";
-    MetadataKeys["validator"] = "validator";
-})(MetadataKeys || (MetadataKeys = {}));
--- myExpress/build/controllers/decorators/Methods.js
+++
@@ -1,8 +0,0 @@
-export var Methods;
-(function (Methods) {
-    Methods["get"] = "get";
-    Methods["post"] = "post";
-    Methods["patch"] = "patch";
-    Methods["put"] = "put";
-    Methods["del"] = "delete";
-})(Methods || (Methods = {}));
